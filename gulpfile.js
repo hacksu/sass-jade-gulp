35a16b60db0a990318840193d03350587229c362
Paul Dilyard <paul.dilyard@gmail.com>
2015-10-12 20:48:32 -0400

    Requires in gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp');
+const jade = require('gulp-jade');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const serve = require('gulp-serve');
+const watch = require('gulp-watch');
+
