8a4b62d266845da26fa92c3e9461482dbd5e75df
Paul Dilyard <paul.dilyard@gmail.com>
2015-10-13 20:22:47 -0400

    End of talk

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,26 @@
-const gulp = require('gulp');
-const jade = require('gulp-jade');
-const plumber = require('gulp-plumber');
-const sass = require('gulp-sass');
-const serve = require('gulp-serve');
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var serve = require('gulp-serve');
 
+gulp.task('templates', function () {
+  return gulp.src('templates/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['templates', 'styles']);
+
+gulp.task('default', ['build'], function () {
+  gulp.watch(
+    ['styles/*.scss', 'templates/*.jade'],
+    ['build']
+  );
+});
